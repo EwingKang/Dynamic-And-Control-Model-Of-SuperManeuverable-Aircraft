e1686b50b149d4651f8ba1ab6b08e1a98782946c
Ewing Kang <f039281310@yahoo.com.tw>
2016-07-12 15:31:10 +0800

    v0.3.1: fix rotational kinematics bug, add liscence and info

--- X31 model/X31dynamics_03_init.m
+++ X31 model/X31dynamics_03_init.m
@@ -1,3 +1,70 @@
+%{
+****************************** DISCLAIMER *********************************
+    I, ±d¤ß«³, personally create and debug all parts of this simulation program.
+However, it is not me nor anyone that truely "invent" the numerical aircraft 
+model like this one. It is based on the long history of all mathematical, 
+science, and engineering of humanity. Like Isaac Newton said, "standing on
+ the shoulders of giants".
+    The aerodynamical parameter of the vehicle in this project comes from 
+the thesis written by Snell [1]. In his work, he collected and estimated the
+data from multiple source including all major aero-astro research and develop
+institude around the world. Also, the method to model the dynamics of the 
+vehicle and the controller implemented including the NDI and Gain Scheduling 
+also came from his thesis.              
+    The quaternion kinematics mainly reference to the toturial paper done by 
+Trawny [2], where he collected and organized all fundamental properties and
+mathematical operation of such system.                   
+    This model is released to improve our ability to understand and develop 
+control methodology of aerial vehicle system similar to this one. It is 
+released free of charge now and always will be so in the hope of sharing the
+knowledge as much as possible and making this world a better place. 
+    However, despite my greatest effort to ensure the correctness of the 
+code, it is not garantee to be free of bug or other issue. You are free to 
+use and modify the program to meet your need but I'm not responsible for any
+potential risk or damaged of any kind.
+    Long live and prosper
+***************************** Liscence: GPLv2 ****************************
+Dynamic Model of Super-Maneuverable Aircraft
+Copyright (C) 2016 Hsin-Yi Kang
+DAA, NCKU
+f039281310@yahoo.com.tw
+This program is free software; you can redistribute it and/or modify it   
+under the terms of the GNU General Public License as published by the Free 
+Software Foundation; either version 2 of the License, or (at your option) 
+any later version. This program is distributed in the hope that it will be 
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 
+Public License for more details. You should have received a copy of the GNU 
+General Public License along with this program; if not, write to the Free 
+Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+MA 02110-1301, USA. 
+**************************** Main Reference *******************************
+X31:
+    [1] Snell, Sidney Antony, "Nonlinear Dynamic-Inversion Flight Control 
+of Supermaneuverable Aircraft", Ph.D thesis, UoMinnesota, 1991
+Quaternion:
+    [2] Nikolas Trawny and StergiosI. Roumeliotis, "Indirect Kalman Filter 
+for 3D Attitude Estimation," University of Minnesota, 2005.
+                         ***Side Reference***
+X31:
+    [3] David F. Fisher, John H. Del Frate, and David M. Richwinc, "In-Flight 
+Flow Visualization Characteristics of the NASA F-18 High Alpha Research 
+Vehicle at High Angles of Attack", Technical Memorandum4193, NASA, Edwards, 
+CA, 1990.
+    [4] W. B. W. a. W. P. Fellers, "Tail Configurations for Highly 
+Maneuverable Aircraft," AGARD, 1981.
+
+Quaternion:
+    [5] Nikolas Trawnyand StergiosI. Roumeliotis, "Indirect Kalman Filter 
+for 3D Attitude Estimation," University of Minnesota, 2005.
+    [6] W. G. Breckenridge, "Quaternions -Proposed Standard Conventions" 
+JPL, Tech. Rep. INTEROFFICE MEMORANDUM 343-79-1199, 1999.
+    [7] D. M. Henderson, "EularAngles, Quaternions, and Transformation 
+Matrices," Mission Planning and Analysis Division, NASA, July 1977.
+%}
+
+
+
 function X31dynamics_03_init()
 %THIS NEED TO BE PROGRAM TO BE REAL FUNCTIONAL AND MORE INPUT FRIENDLY
 
